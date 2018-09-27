46e5e90978b2967e20b19d5bf339452e58dc9cd0
Thran <thran@centrum.cz>
2018-09-27 20:51:32 +0200

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        ScrollToModule.forRoot(),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
