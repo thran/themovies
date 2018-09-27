1eca72dd94497f26cdbcd1227d429f51c7fd6175
Thran <thran@centrum.cz>
2018-09-27 21:11:40 +0200

    changes for build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies';
+    active1 = false;
+    active2 = false;
+    active3 = false;
+    active4 = false;
+    active5 = false;
+    active6 = false;
+    active7 = false;
+    active8 = false;
+    active9 = false;
 }
